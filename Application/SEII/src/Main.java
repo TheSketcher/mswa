4314c88555bcfc3c36fc6d5d478d593602d118ad
Christopher Caspari <caspari.christop.a21@student.dhbw-Karlsruhe.de>
2023-03-24 16:11:38 +0100

    VehicleComponentsCom implementiert

--- Application/SEII/src/Main.java
+++ Application/SEII/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            VehicleComponentsCom.sendPostRequest();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
